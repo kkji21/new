00bdd9b214158681b74803e62154ac9d63bd9a7a
kk <kkjiweb@gmail.com>
2021-10-20 16:23:21 +0530

    done

--- proj/settings.py
+++ proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-86c(m-gl7+1&vc3vex8g70_l$x$_bxit=ul9fcc@d=_x_z18f9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'https://kkji21.github.io/new/']
+ALLOWED_HOSTS = ['127.0.0.1' , 'https://kkji21.github.io/new/templates/']
 
 
 # Application definition
